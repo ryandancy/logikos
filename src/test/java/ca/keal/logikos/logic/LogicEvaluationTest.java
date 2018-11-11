7f5e65164fd553b1a4aeefe58936a0e8da0b7829
Ryan Dancy <ryan@keal.ca>
2018-11-11 18:48:10 -0500

    Add tests for one-gate circuits
    There are now simple truth-table parameterized tests for the built-in
    NOT, AND, OR, and NAND gates. All are simply Inputs connected to the
    gate which is connected to an Output.

--- src/test/java/ca/keal/logikos/logic/LogicEvaluationTest.java
+++ src/test/java/ca/keal/logikos/logic/LogicEvaluationTest.java
@@ -21,4 +21,77 @@ class LogicEvaluationTest {
     assertArrayEquals(output.evaluate(), new boolean[] {value});
   }
   
+  @DisplayName("Simple NOT gate circuit")
+  @ParameterizedTest(name = "NOT {0} should be {1}")
+  @CsvSource({"true, false", "false, true"})
+  void notGate(boolean inputValue, boolean outputValue) {
+    Input input = new Input();
+    NotGate not = new NotGate();
+    Output output = new Output();
+    
+    input.getOutput(0).connectTo(not.getInput(0));
+    not.getOutput(0).connectTo(output.getInput(0));
+    input.setValue(inputValue);
+    
+    assertArrayEquals(output.evaluate(), new boolean[] {outputValue});
+  }
+  
+  @DisplayName("Simple AND gate circuit")
+  @ParameterizedTest(name = "{0} AND {1} should be {2}")
+  @CsvSource({"true, true, true", "true, false, false", "false, true, false", "false, false, false"})
+  void andGate(boolean inputVal1, boolean inputVal2, boolean outputVal) {
+    Input input1 = new Input();
+    Input input2 = new Input();
+    AndGate and = new AndGate();
+    Output output = new Output();
+    
+    input1.getOutput(0).connectTo(and.getInput(0));
+    input2.getOutput(0).connectTo(and.getInput(1));
+    and.getOutput(0).connectTo(output.getInput(0));
+    
+    input1.setValue(inputVal1);
+    input2.setValue(inputVal2);
+    
+    assertArrayEquals(output.evaluate(), new boolean[] {outputVal});
+  }
+  
+  @DisplayName("Simple OR gate circuit")
+  @ParameterizedTest(name = "{0} OR {1} should be {2}")
+  @CsvSource({"true, true, true", "true, false, true", "false, true, true", "false, false, false"})
+  void orGate(boolean inputVal1, boolean inputVal2, boolean outputVal) {
+    Input input1 = new Input();
+    Input input2 = new Input();
+    OrGate or = new OrGate();
+    Output output = new Output();
+    
+    input1.getOutput(0).connectTo(or.getInput(0));
+    input2.getOutput(0).connectTo(or.getInput(1));
+    or.getOutput(0).connectTo(output.getInput(0));
+    
+    input1.setValue(inputVal1);
+    input2.setValue(inputVal2);
+    
+    assertArrayEquals(output.evaluate(), new boolean[] {outputVal});
+  }
+  
+  
+  @DisplayName("Simple NAND gate circuit")
+  @ParameterizedTest(name = "{0} NAND {1} should be {2}")
+  @CsvSource({"true, true, false", "true, false, true", "false, true, true", "false, false, true"})
+  void nandGate(boolean inputVal1, boolean inputVal2, boolean outputVal) {
+    Input input1 = new Input();
+    Input input2 = new Input();
+    NandGate nand = new NandGate();
+    Output output = new Output();
+    
+    input1.getOutput(0).connectTo(nand.getInput(0));
+    input2.getOutput(0).connectTo(nand.getInput(1));
+    nand.getOutput(0).connectTo(output.getInput(0));
+    
+    input1.setValue(inputVal1);
+    input2.setValue(inputVal2);
+    
+    assertArrayEquals(output.evaluate(), new boolean[] {outputVal});
+  }
+  
 }
