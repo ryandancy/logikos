85e95a239ab4d21abb87f8975d881c034b3bea7d
Ryan Dancy <ryan@keal.ca>
2018-12-13 18:39:23 -0500

    Add UserGate
    UserGate is a gate customized by the user. It contains a Field to which
    it delegates evaluation. UserGates will be creatable by the user based
    on a Field. They will also be savable.
    
    As well, Field now maintains Lists of its InputFCs and OutputFCs.
    getInputFCs() and getOutputFCs() now return unmodifiable versions of
    these lists instead of delegating to a common reflection-based
    generic method. This was done so that Field has an order to its
    InputFCs and OutputFCs so that UserGate can determine which input to
    set to which InputFCs. Field now also has an evaluate() method which
    simply returns an array of the results of the evaluation of each
    OutputFC in order.

--- src/main/java/ca/keal/logikos/field/Field.java
+++ src/main/java/ca/keal/logikos/field/Field.java
@@ -1,9 +1,10 @@
 package ca.keal.logikos.field;
 
+import ca.keal.logikos.logic.EvaluationListener;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * A Field is the container for the entire logic model. It contains {@link FieldComponent}s and handles saving and
@@ -13,35 +14,59 @@ public class Field {
   
   private final List<FieldComponent> fieldComponents = new ArrayList<>();
   
+  // These lists exist to provide an order for the inputFCs
+  // TODO some way to reorder the inputs/outputs
+  private final List<InputFC> inputFCs = new ArrayList<>();
+  private final List<OutputFC> outputFCs = new ArrayList<>();
+  
   public List<FieldComponent> getFieldComponents() {
     return Collections.unmodifiableList(fieldComponents);
   }
   
   public List<InputFC> getInputFCs() {
-    return getFCsOfType(InputFC.class);
+    return Collections.unmodifiableList(inputFCs);
   }
   
   public List<OutputFC> getOutputFCs() {
-    return getFCsOfType(OutputFC.class);
+    return Collections.unmodifiableList(outputFCs);
   }
   
-  private <FC extends FieldComponent> List<FC> getFCsOfType(Class<FC> fcClass) {
-    //noinspection unchecked - why does `(FC) fc` even generate a warning? erasure is weird
-    return fieldComponents.stream()
-        .filter(fc -> fcClass.isAssignableFrom(fc.getClass()))
-        .map(fc -> (FC) fc)
-        .collect(Collectors.toList());
-  }
-  
-  public boolean addFieldComponent(FieldComponent component) {
+  public void addFieldComponent(FieldComponent component) {
     if (component == null) {
       throw new NullPointerException("A null FieldComponent cannot be added to a field");
     }
-    return fieldComponents.add(component);
+    
+    fieldComponents.add(component);
+    
+    if (component instanceof InputFC) {
+      inputFCs.add((InputFC) component);
+    } else if (component instanceof OutputFC) {
+      outputFCs.add((OutputFC) component);
+    }
   }
   
-  public boolean removeFieldComponent(FieldComponent component) {
-    return fieldComponents.remove(component);
+  public void removeFieldComponent(FieldComponent component) {
+    fieldComponents.remove(component);
+    if (component instanceof InputFC) {
+      inputFCs.remove(component);
+    } else if (component instanceof OutputFC) {
+      outputFCs.remove(component);
+    }
+  }
+  
+  /**
+   * Evaluate the entire {@link Field} and return the output as an array.
+   * @param listener An {@link EvaluationListener} to listen to each gate's evaluation. May be {@code null}.
+   * @return An array containing the output of each {@link OutputFC} in order. To get the {@link OutputFC} associated
+   *  with each result, match each element in the returned array with the corresponding element in
+   *  {@link #getOutputFCs()}.
+   */
+  public boolean[] evaluate(EvaluationListener listener) {
+    boolean[] output = new boolean[outputFCs.size()];
+    for (int i = 0; i < outputFCs.size(); i++) {
+      output[i] = outputFCs.get(i).evaluate(listener);
+    }
+    return output;
   }
   
 }
---
+++ src/main/java/ca/keal/logikos/logic/UserGate.java
@@ -0,0 +1,69 @@
+package ca.keal.logikos.logic;
+
+import ca.keal.logikos.field.Field;
+import ca.keal.logikos.field.InputFC;
+
+import java.util.List;
+
+/**
+ * A user-created gate based on a {@link Field}. The {@code UserGate} is a way for users to create a custom gate based
+ * on a {@link Field} they have previously created. It contains a {@code Field} to which it delegates evaluation. It
+ * also has a user-entered name.
+ */
+public class UserGate extends Gate {
+  
+  private Field field;
+  private String name;
+  
+  /**
+   * Create a {@link UserGate} based on the given {@link Field}. The new {@code UserGate} is functionally identical to
+   * the {@code Field} when run.
+   * @param field The {@link Field} on which to base this {@code UserGate}.
+   * @param name The name of the new {@code UserGate}.
+   */
+  protected UserGate(Field field, String name) {
+    super(field.getInputFCs().size(), field.getOutputFCs().size());
+    setField(field);
+    setUserGateName(name);
+  }
+  
+  public void setField(Field field) {
+    if (field == null) {
+      throw new NullPointerException("UserGate cannot have a null Field");
+    }
+    this.field = field;
+  }
+  
+  public Field getField() {
+    return field;
+  }
+  
+  public void setUserGateName(String name) {
+    if (name == null) {
+      throw new NullPointerException("UserGate cannot have a null name");
+    }
+    this.name = name;
+  }
+  
+  public String getUserGateName() {
+    return name;
+  }
+  
+  @Override
+  protected boolean[] logicalEval(boolean[] input) {
+    // Set the inputs
+    List<InputFC> inputFCs = field.getInputFCs();
+    for (int i = 0; i < input.length; i++) {
+      inputFCs.get(i).getLogicComponent().setValue(input[i]);
+    }
+    
+    // Evaluate
+    return field.evaluate(null);
+  }
+  
+  @Override
+  protected String getName() {
+    return "UserGate('" + getUserGateName() + "')";
+  }
+  
+}
