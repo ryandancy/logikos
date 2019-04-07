65edc6d9b461a154f1548cbc05465902ac11de29
Ryan Dancy <ryan@keal.ca>
2019-04-06 21:21:14 -0400

    Make Position implement Saveable
    The Position of field components must be saved, so Position has been
    made Saveable. It saves its x- and y-coordinates in two elements, <x>
    and <y>. Its tag is <position>.

--- src/main/java/ca/keal/logikos/field/Position.java
+++ src/main/java/ca/keal/logikos/field/Position.java
@@ -1,16 +1,26 @@
 package ca.keal.logikos.field;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 /**
  * A position on the field.
  */
-public class Position {
+public class Position implements Saveable {
   
-  private final double x;
-  private final double y;
+  // effectively final, but not officially final for Saveable
+  private double x;
+  private double y;
   
   public Position(double x, double y) {
     this.x = x;
     this.y = y;
+  }
+  
+  /** Fulfilling the Saveable contract */
+  private Position() {
+    x = Double.NaN;
+    y = Double.NaN;
   }
   
   public double getX() {
@@ -19,6 +29,28 @@ public class Position {
   
   public double getY() {
     return y;
+  }
+  
+  @Override
+  public void save(Document dom, Element serialized) {
+    Element xElem = dom.createElement("x");
+    xElem.setTextContent(String.valueOf(x));
+    serialized.appendChild(xElem);
+    
+    Element yElem = dom.createElement("y");
+    yElem.setTextContent(String.valueOf(y));
+    serialized.appendChild(yElem);
+  }
+  
+  @Override
+  public void populate(Element serialized) {
+    x = Double.parseDouble(serialized.getElementsByTagName("x").item(0).getTextContent());
+    y = Double.parseDouble(serialized.getElementsByTagName("y").item(0).getTextContent());
+  }
+  
+  @Override
+  public String getElementName() {
+    return "position";
   }
   
   @Override
